eb8a670c74c59fe2277e25138ef750b8b1b09f4d
benaiah-ke <benaiahpraise@gmail.com>
2022-08-28 13:56:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards (array, event) {
+    let messages = [];
+
+    for (let i = 0; i < array.length; i++) {
+        console.log (`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        messages.push (`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+};
+
+// console.log (writeCards(['benaiah','praise', 'wepundi'], 'birthday'));
+
+function countDown (number) {
+    while (number !== -1) {
+        console.log (number--);
+    };
+}
